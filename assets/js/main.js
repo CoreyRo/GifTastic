080db0ce954ae5302a1daee751f6231a6879433c
Corey Rodems <coreyrodems@gmail.com>
2017-07-21 18:43:41 -0700

    api call, animate on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,26 @@ $(document).ready(function($) {
 
 
 
-
+	var apiKey = "dc6zaTOxFJmzC";
+	var limit = 12;
 	var recent = ["McCree", "POTG", "Fail", "The Big Lebowski"];
 	// var queryURL = "http://api.giphy.com/v1/gifs/search?" + value + "&api_key=dc6zaTOxFJmzC&limit=5";
+	function playGif(played){
+		
+		var state = $(this).attr("data-state");
+    	var animated = $(this).attr("data-animate");
+    	var still  = $(this).attr("data-still");
 
-	function alertgifName(value) {
-		value = $(this).attr('data-name').replace(" ", "+");
-		console.log(value);
-	}
+    	if(state === "still"){
+	    	$(this).attr("data-state", "animate");
+	    	$(this).attr("src", animated);
+    	}
+    	else{
+    		$(this).attr("data-state", "still");
+	    	$(this).attr("src", still);
+    	}
+    	
+    }
 
 
 	function renderButtons() {
@@ -24,14 +36,74 @@ $(document).ready(function($) {
 		}
 	}
 
+	function gifButton(value){
+	$("#gif-view").empty();
+	value = $(this).attr('data-name').replace(" ", "+");
+	console.log(value);
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + value + "&api_key=" + apiKey + "&limit=" + limit;
+	
+	// var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=" + apiKey + "&tag=" + value;
+
+	console.log(queryURL);
+	$.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .done(function(response) {
+        var results = response.data;
+        console.log(results);
+        for(var i = 0;i < results.length; i++){
+        	var gifDiv = $("<div class='col-md-4 item'>");
+            var gifStill = results[i].images.fixed_height_still.url;
+            console.log(gifStill)
+
+            
+
+            var gifImg = $("<img class='center-block still'>");
+            gifImg.attr("data-still", results[i].images.fixed_height_still.url)
+            gifImg.attr("data-animate", results[i].images.fixed_height.url)
+            gifImg.attr("data-state", "still");
+            gifImg.attr("src", results[i].images.fixed_height_still.url);
+            console.log
+           	
+            gifDiv.prepend(gifImg);
+
+            $("#gif-view").prepend(gifDiv);
+        }
+        $(document).on("click", "img.still", playGif);
+        
+
+	});
+	
+    
+
+	}
+
+	// $.ajax({
+ //        url: queryURL,
+ //        method: "GET"
+ //    })
+ //    .done(function(response) {
+ //        var results = response.data;
+
+ //        
+ //    });
+	// }
+
+	$(document).on("click", "button.recentgif", gifButton);
+	
+
+
+	
 
 	$("#add-gif").on("click", function(event) {
 		event.preventDefault();
+		console.log(recent);
 		var value = $("#gif-input").val().trim();
 		console.log(value);
 		recent.push(value);
 		renderButtons();
-
+	
 	});
 
 		renderButtons();
